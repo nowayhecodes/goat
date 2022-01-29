e96f63fd44ff46bce7d4e78b15ad71f421f9c8a8
Gustavo Cavalcante <nowayhecodes@gmail.com>
2022-01-29 19:49:15 -0300

    chore: add Context struct

---
+++ context.go
@@ -0,0 +1,19 @@
+package goat
+
+import (
+	"net/http"
+)
+
+type H map[string]interface{}
+
+type Context struct {
+	Writer     http.ResponseWriter
+	Req        *http.Request
+	Path       string
+	Method     string
+	Params     map[string]string
+	StatusCode int
+	handlers   []FuncHandler
+	index      int
+	engine     *Engine
+}
