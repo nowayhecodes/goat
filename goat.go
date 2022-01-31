233cf41b523ccadf01ae19900702c484120ab423
Gustavo Cavalcante <nowayhecodes@gmail.com>
2022-01-31 03:07:01 -0300

    feat: static handler + minor refactoring

--- goat.go
+++ goat.go
@@ -3,14 +3,16 @@ package goat
 import (
 	"html/template"
 	"log"
+	"net/http"
+	"path"
 )
 
-type HandlersFn func(*Context)
+type HandlerFn func(*Context)
 
 type (
 	RouterGroup struct {
 		prefix      string
-		middlewares []HandlersFn
+		middlewares []HandlerFn
 		parent      *RouterGroup
 		engine      *Engine
 	}
@@ -49,40 +51,55 @@ func (group *RouterGroup) Group(prefix string) *RouterGroup {
 	return newGroup
 }
 
-func (group *RouterGroup) Use(middlewares ...HandlersFn) {
+func (group *RouterGroup) Use(middlewares ...HandlerFn) {
 	group.middlewares = append(group.middlewares, middlewares...)
 }
 
-func (group *RouterGroup) addRoute(method string, comp string, handler HandlersFn) {
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFn) {
 	pattern := group.prefix + comp
 	log.Printf("Route %4s - %s", method, pattern)
 	group.engine.router.addRoute(method, pattern, handler)
 }
 
-func (group *RouterGroup) GET(pattern string, handler HandlersFn) {
+func (group *RouterGroup) GET(pattern string, handler HandlerFn) {
 	group.addRoute("GET", pattern, handler)
 }
 
-func (group *RouterGroup) POST(pattern string, handler HandlersFn) {
+func (group *RouterGroup) POST(pattern string, handler HandlerFn) {
 	group.addRoute("POST", pattern, handler)
 }
 
-func (group *RouterGroup) PUT(pattern string, handler HandlersFn) {
+func (group *RouterGroup) PUT(pattern string, handler HandlerFn) {
 	group.addRoute("PUT", pattern, handler)
 }
 
-func (group *RouterGroup) PATCH(pattern string, handler HandlersFn) {
+func (group *RouterGroup) PATCH(pattern string, handler HandlerFn) {
 	group.addRoute("PATCH", pattern, handler)
 }
 
-func (group *RouterGroup) DELETE(pattern string, handler HandlersFn) {
+func (group *RouterGroup) DELETE(pattern string, handler HandlerFn) {
 	group.addRoute("DELETE", pattern, handler)
 }
 
-func (group *RouterGroup) OPTIONS(pattern string, handler HandlersFn) {
+func (group *RouterGroup) OPTIONS(pattern string, handler HandlerFn) {
 	group.addRoute("OPTIONS", pattern, handler)
 }
 
-func (group *RouterGroup) HEAD(pattern string, handler HandlersFn) {
+func (group *RouterGroup) HEAD(pattern string, handler HandlerFn) {
 	group.addRoute("HEAD", pattern, handler)
+}
+
+func (group *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFn {
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+
+	return func(ctx *Context) {
+		file := ctx.Param("filePath")
+		if _, err := fs.Open(file); err != nil {
+			ctx.Status(http.StatusNotFound)
+			return
+		}
+
+		fileServer.ServeHTTP(ctx.Writer, ctx.Req)
+	}
 }
